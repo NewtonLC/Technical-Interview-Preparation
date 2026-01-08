0e823757a2baced5da17bf4f3ef1e3b1d5c4940c
Newton Chung <newtonchung99@gmail.com>
2026-01-07 23:12:15 -0800

    Completed Binary Search

--- Neetcode/binary_search/Binary Search/question.md
+++ Neetcode/binary_search/Binary Search/question.md
@@ -1,16 +1,27 @@
-Difficulty: [To be filled]
+Difficulty: Easy
 
-[Problem description to be added]
+You are given an array of distinct integers nums, sorted in ascending order, and an integer target.
+
+Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
+
+Your solution must run in 
+O(logn) time.
 
 Example 1:
 ```
-Input: [Input description]
+Input: nums = [-1,0,2,4,6,8], target = 4
 
-Output: [Output description]
+Output: 3
 ```
 Example 2:
 ```
-Input: [Input description]
+Input: nums = [-1,0,2,4,6,8], target = 3
 
-Output: [Output description]
+Output: -1
 ```
+
+Constraints:
+
+- 1 <= nums.length <= 10000.
+- -10000 < nums[i], target < 10000
+- All the integers in nums are unique.
--- Neetcode/binary_search/Binary Search/submission_1.py
+++ Neetcode/binary_search/Binary Search/submission_1.py
@@ -1,20 +1,54 @@
 """
-Date: [To be filled]
-Memory: [To be filled]
-Runtime: [To be filled]
+Date: 01/07/2026 23:08
+Memory: O(1)
+Runtime: O(logn)
 """
 
 """
 class Solution:
-    def methodName(self, parameters):
-        # Implementation to be added
-        pass
+    def search(self, nums: List[int], target: int) -> int:
+        # In: int[] nums, sorted and distinct. int target
+        # Out: int, index of target OR -1 if target isn't found
+        # Con:
+        # - Solution must run in O(logn) time --> Must use BST
+        # Edge:
+        # - Empty array? --> Not possible
+        # - Invalid input? --> Not possible
+
+        left = 0
+        right = len(nums)-1
+        mid = (left+right)//2
+
+        while left <= right:
+            curr_num = nums[mid]
+            if curr_num < target:
+                left = mid+1
+            elif curr_num > target:
+                right = mid-1
+            else:
+                return mid
+            mid = (left+right)//2
+        
+        return -1
 """
 
-def function_name(parameters):
-    # Implementation to be added
-    pass
+def search(nums, target):
+    left = 0
+    right = len(nums)-1
+    mid = (left+right)//2
+
+    while left <= right:
+        curr_num = nums[mid]
+        if curr_num < target:
+            left = mid+1
+        elif curr_num > target:
+            right = mid-1
+        else:
+            return mid
+        mid = (left+right)//2
+    
+    return -1
 
 # Test cases to be added
-# print(function_name(test_input))  # Expected: expected_output
-# print(function_name(test_input))  # Expected: expected_output
+print(search([-1,0,2,4,6,8],4)) # 3
+print(search([-1,0,2,4,6,8],3)) # -1
