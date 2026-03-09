4a11f90a287c4ce6d43651d886ec060c1160f610
Newton Chung <newtonchung99@gmail.com>
2026-03-09 15:50:33 -0700

    Solved Kth Largest Element In An Array

--- Neetcode/heap_queue/Kth Largest Element In An Array/question.md
+++ Neetcode/heap_queue/Kth Largest Element In An Array/question.md
@@ -1,16 +1,24 @@
-Difficulty: [To be filled]
+Difficulty: Medium
 
-[Problem description to be added]
+Given an unsorted array of integers nums and an integer k, return the kth largest element in the array.
+
+By kth largest element, we mean the kth largest element in the sorted order, not the kth distinct element.
+
+Follow-up: Can you solve it without sorting?
 
 Example 1:
 ```
-Input: [Input description]
+Input: nums = [2,3,1,5,4], k = 2
 
-Output: [Output description]
+Output: 4
 ```
 Example 2:
 ```
-Input: [Input description]
+Input: nums = [2,3,1,1,5,5,4], k = 3
 
-Output: [Output description]
+Output: 4
 ```
+
+Constraints:
+- 1 <= k <= nums.length <= 10000
+- -1000 <= nums[i] <= 1000
--- Neetcode/heap_queue/Kth Largest Element In An Array/submission_1.py
+++ Neetcode/heap_queue/Kth Largest Element In An Array/submission_1.py
@@ -1,20 +1,72 @@
 """
-Date: [To be filled]
-Memory: [To be filled]
-Runtime: [To be filled]
+Date: 03/09/2026 15:43
+Memory: 28ms (Beats 100%)       O(n log k)
+Runtime: 7.7 MB (Beats 100%)    O(k)
 """
 
 """
+import heapq
+
 class Solution:
-    def methodName(self, parameters):
-        # Implementation to be added
-        pass
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # In: int[] nums unsorted, int k
+
+        # Out: int --> kth largest element in the array
+
+        # Con:
+        # - 1 <= k <= nums.length <= 10000 --> k cannot be out-of-bounds.
+        # - Numbers are not always distinct
+
+        # Ex:
+        # - Input: nums = [2,3,1,5,4], k = 2
+        # - Output: 4, 4 is the 2nd-largest number in nums.
+
+        # Approach 1: Sort
+        # Sort nums() --> nums = sort(nums)
+        # Take the k-th element.
+        # Time: O(n logn)
+        # Space: O(1)
+
+        # Approach 2: Min-Heap
+        # The Min Heap will store k-number of elements
+        # It stores elements and keeps the smallest element at the top.
+        # When we add an element to the Min-Heap, it takes O(logk) time since we store k elements in it.
+        # Retrieving the top element takes O(1) time since it's at the top.
+        # We plan to store the largest elements from nums in this min-heap, 1st - kth.
+            # At the end of the algorithm, the kth largest element will be the smallest element in the min-heap.
+
+        # When looping through nums, if we encounter a number smaller than the top of minHeap...
+            # It's not relevant to our answer at all, so we can ignore it.
+        # If we encounter a number larger than the minHeap...
+            # We want to add this number to our heap, but also remove the top of the heap.
+                # We can perform this function most efficiently by running heapq.heappushpop(minHeap, num)
+        # At the end of the loop, return heapq.pop(minHeap)
+
+        # Let's do the minHeap approach.
+
+        # Initialize minHeap to be the first k elements of nums.
+        minHeap = nums[:k]
+        heapq.heapify(minHeap)
+
+        for num in nums[k:]:
+            if num > minHeap[0]:
+                heapq.heappushpop(minHeap, num)
+        
+        return heapq.heappop(minHeap)
 """
 
-def function_name(parameters):
-    # Implementation to be added
-    pass
+import heapq
+
+def findKthlargest(nums, k):
+    minHeap = nums[:k]
+    heapq.heapify(minHeap)
+
+    for num in nums[k:]:
+        if num > minHeap[0]:
+            heapq.heappushpop(minHeap, num)
+    
+    return heapq.heappop(minHeap)
 
 # Test cases to be added
-# print(function_name(test_input))  # Expected: expected_output
-# print(function_name(test_input))  # Expected: expected_output
+print(findKthlargest([2,3,1,5,4], 2))  # Expected: 4
+print(findKthlargest([2,3,1,1,5,5,4], 3))  # Expected: 4
